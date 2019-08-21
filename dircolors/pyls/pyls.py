bc68c65823074986784120947f17b21b9533bfcd
Allen Wild <allenwild93@gmail.com>
2019-08-20 23:08:39 -0400

    pyls: gracefully handle errors reading or stat-ing a directory
    It's bad to crash and print a backtrace when pyls is run on a file we
    don't  have permissions to read.

--- dircolors/pyls/pyls.py
+++ dircolors/pyls/pyls.py
@@ -19,11 +19,14 @@ def main():
 
     dc = Dircolors()
     for f in files:
-        if os.path.isdir(f) and not os.path.islink(f):
-            if f != '.':
-                print(dc.format(f) + ':')
-            for ff in sorted(os.listdir(f)):
-                print(dc.format(ff, f, show_target=True))
-            print()
-        else:
-            print(dc.format(f, show_target=True))
+        try:
+            if os.path.isdir(f) and not os.path.islink(f):
+                if f != '.':
+                    print(dc.format(f) + ':')
+                for ff in sorted(os.listdir(f)):
+                    print(dc.format(ff, f, show_target=True))
+                print()
+            else:
+                print(dc.format(f, show_target=True))
+        except OSError as e:
+            print('%s: error: %s'%(f, e), file=sys.stderr)
